71f3601837024ea4790ce85dd442b01f41e0f87d
Bruno Silva <126625827+BruSilva04@users.noreply.github.com>
2023-08-16 16:06:55 -0300

    Update script.js

--- RisoOdonto/js/script.js
+++ RisoOdonto/js/script.js
@@ -1,7 +1,9 @@
 // Função para atualizar a classe ativa da barra de navegação com base na posição da página
 function updateActiveSection() {
-  const sections = document.querySelectorAll('section'); // Seleciona todas as seções da página
-  const navLinks = document.querySelectorAll('.nav-link'); // Seleciona os links da barra de navegação
+  // Seleciona todas as seções da página
+  const sections = document.querySelectorAll('section'); 
+  // Seleciona os links da barra de navegação
+  const navLinks = document.querySelectorAll('.nav-link'); 
 
   sections.forEach((section, index) => {
     const sectionTop = section.offsetTop;
